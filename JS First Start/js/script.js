beba413cf0eb60e25875012dc297a4afd7d9a4fe
Artemiy228 <t3ma2002@mail.ru>
2020-06-09 21:00:35 +0300

    if / else / else if

--- JS First Start/js/script.js
+++ JS First Start/js/script.js
@@ -24,12 +24,28 @@ const personalMovieDB = {
 	gentres: [],
 	privat: false
 };
-const a = prompt('Один из последних просмотренных фильмов? ', ''),
-	b = prompt('На сколько вы его оцените? ', ''),
-	c = prompt('Один из последних просмотренных фильмов? ', ''),
-	d = prompt('На сколько вы его оцените? ', '');
+for (let i = 1; i <= 2; i++) {
+	const a = prompt('Один из последних просмотренных фильмов? ', ''),
+		b = prompt('На сколько вы его оцените? ', '');
+	if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+		personalMovieDB.movies[a] = b;
+	} else {
+		console.log('error');
+		i--;
+	}
+}
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+
+if (personalMovieDB.count != null && personalMovieDB.count < 10) {
+	console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+	console.log('Вы классический зритель');
+} else if (personalMovieDB.count > 30) {
+	console.log('Вы киноман!');
+} else {
+	console.log('Вы лох!');
+
+}
+
 
 console.log(personalMovieDB);
